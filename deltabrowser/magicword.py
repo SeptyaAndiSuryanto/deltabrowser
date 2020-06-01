eefa251c560a0ab7dd7201d5d83220f1294426a4
SeptyaAndiSuryanto <septyaandi24@gmail.com>
2020-06-01 17:16:16 +0700

    add new workspace

---
+++ deltabrowser/magicword.py
@@ -0,0 +1,2 @@
+# SECURITY WARNING: keep the secret key used in production secret!
+SECRET_KEY = '7izvcql07va39!4ke6hu8gq#%u2bw56qt07g%@%ddbs)ik07i+'
--- deltabrowser/settings.py
+++ deltabrowser/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/3.0/ref/settings/
 """
 
 import os
+from .magicword import *
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -19,8 +20,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.0/howto/deployment/checklist/
 
-# SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '7izvcql07va39!4ke6hu8gq#%u2bw56qt07g%@%ddbs)ik07i+'
+
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
